158d2009ee4dfd3c914dd5f6194c8d9c1c7c7bf4
Sanchit <Sanchit7851@gmail.com>
2025-08-10 11:23:18 +0530

    first demo file created server and dockerfile

---
+++ Server.js
@@ -0,0 +1,13 @@
+const express= require('express');
+const app= express();
+
+
+app.get('/',(req,res)=>{
+    res.send("Helluu from sanchit V1");
+
+});
+
+app.listen(3000, ()=>{
+    console.log("server listening on port 3000");
+
+});
